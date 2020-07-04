493ab363aaff208c5d03bee979b7ece92e5e1c13
Septiana Yoga <septianayoga30@gmail.com>
2020-07-04 13:50:00 +0700

    fixed wrong file edit

---
+++ assets/components/navbar.js
@@ -0,0 +1,14 @@
+const NAVBAR = `
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">World Cup 2018</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Home Page</a></li>
+        <li><a href="#">Team List</a></li>
+        <li><a href="#">Saved Team</a></li>
+      </ul>
+    </div>
+  </nav>
+`;
+
+export default NAVBAR;
