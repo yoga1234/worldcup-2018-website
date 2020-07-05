c753ac58d02f8ca2dcd36095736ea73edeede417
Septiana Yoga <septianayoga30@gmail.com>
2020-07-06 01:47:06 +0700

    update homepage

--- assets/components/pages/homepage.js
+++ assets/components/pages/homepage.js
@@ -8,7 +8,19 @@ const HOMEPAGE = () => {
             <img src="./assets/images/180x180.png" alt="Yogasmara Brand Logo">
             <h3 class="light white-text">FIFA World Cup 2018</h3>
             <h5 class="thin white-text">Find your favourite team of world cup 2018 here.</h5>
+            <div class="divider"></div>
+            <div class="col s6">
+              <em>Start date: 09-09-1995</em>
+              <div class="divider"></div>
+            </div>
+            <div class="col s6">
+              <em>End date: 09-09-1995</em>
+              <div class="divider"></div>
+            </div>
           </div>
+        </div>
+        <div>
+          <h4 class="center-align">Team List</h4>
         </div>
       </div>
     </div>
