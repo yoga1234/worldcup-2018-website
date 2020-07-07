b1deeac29c74d2036c29a51e2acda8eb3fcd34bf
Septiana Yoga <septianayoga30@gmail.com>
2020-07-07 13:20:46 +0700

    Move team list from home page to team list page

--- assets/components/pages/homepage.js
+++ assets/components/pages/homepage.js
@@ -10,59 +10,6 @@ const HOMEPAGE = () => {
             <h5 class="thin white-text">Find your favourite team of world cup 2018 here.</h5>
           </div>
         </div>
-        <div>
-          <h4 class="center-align">Team List</h4>
-          <div class="row">
-            <div class="col s12 m12 l6">
-              <div class="card-panel valign-wrapper">
-                <div class="col s4">
-                  <img src="./assets/images/180x180.png" alt="Country Flag" class="responsive-img">
-                </div>
-                <div class="col s8">
-                  <p>Name: France</p>
-                  <p>TLA: FRA</p>
-                  <a class="waves-effect waves-light btn">Detail</a>
-                </div>
-              </div>
-            </div>
-            <div class="col s12 m12 l6">
-              <div class="card-panel valign-wrapper">
-                <div class="col s4">
-                  <img src="./assets/images/180x180.png" alt="Country Flag" class="responsive-img">
-                </div>
-                <div class="col s8">
-                  <p>Name: France</p>
-                  <p>TLA: FRA</p>
-                  <a class="waves-effect waves-light btn">Detail</a>
-                </div>
-              </div>
-            </div>
-            <div class="col s12 m12 l6">
-              <div class="card-panel valign-wrapper">
-                <div class="col s4">
-                  <img src="./assets/images/180x180.png" alt="Country Flag" class="responsive-img">
-                </div>
-                <div class="col s8">
-                  <p>Name: France</p>
-                  <p>TLA: FRA</p>
-                  <a class="waves-effect waves-light btn">Detail</a>
-                </div>
-              </div>
-            </div>
-            <div class="col s12 m12 l6">
-              <div class="card-panel valign-wrapper">
-                <div class="col s4">
-                  <img src="./assets/images/180x180.png" alt="Country Flag" title="images not found" class="responsive-img">
-                </div>
-                <div class="col s8">
-                  <p>Name: France</p>
-                  <p>TLA: FRA</p>
-                  <a class="waves-effect waves-light btn">Detail</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   `;
--- assets/components/pages/teamlist.js
+++ assets/components/pages/teamlist.js
@@ -1,9 +1,59 @@
 const TEAMLIST = () => {
   window.location.hash = "teamlist";
   return `
-  <div>
-    <h1>Team list</h1>
-  </div>
+    <div class="container">
+      <h4 class="center-align">Team List</h4>
+      <div class="row">
+        <div class="col s12 m12 l6">
+          <div class="card-panel valign-wrapper">
+            <div class="col s4">
+              <img src="./assets/images/180x180.png" alt="Country Flag" class="responsive-img">
+            </div>
+            <div class="col s8">
+              <p>Name: France</p>
+              <p>TLA: FRA</p>
+              <a class="waves-effect waves-light btn">Detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m12 l6">
+          <div class="card-panel valign-wrapper">
+            <div class="col s4">
+              <img src="./assets/images/180x180.png" alt="Country Flag" class="responsive-img">
+            </div>
+            <div class="col s8">
+              <p>Name: France</p>
+              <p>TLA: FRA</p>
+              <a class="waves-effect waves-light btn">Detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m12 l6">
+          <div class="card-panel valign-wrapper">
+            <div class="col s4">
+              <img src="./assets/images/180x180.png" alt="Country Flag" class="responsive-img">
+            </div>
+            <div class="col s8">
+              <p>Name: France</p>
+              <p>TLA: FRA</p>
+              <a class="waves-effect waves-light btn">Detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m12 l6">
+          <div class="card-panel valign-wrapper">
+            <div class="col s4">
+              <img src="./assets/images/180x180.png" alt="Country Flag" title="images not found" class="responsive-img">
+            </div>
+            <div class="col s8">
+              <p>Name: France</p>
+              <p>TLA: FRA</p>
+              <a class="waves-effect waves-light btn">Detail</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 `;
 }
 
