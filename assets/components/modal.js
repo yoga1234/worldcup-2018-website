81c8e5514db0e07f70d1c02542a60c0d8d3df4cf
Septiana Yoga <septianayoga30@gmail.com>
2020-08-05 13:48:53 +0700

    adding idb 2.1.3

--- assets/components/modal.js
+++ assets/components/modal.js
@@ -1,43 +1,43 @@
 const MODAL = `
-<div id="team-modal" class="modal modal-fixed-footer">
+<div id="team-modal" class="modal modal-fixed-footer margin-top-50">
   <div class="modal-content">
-    <div class="card-panel teal">
-      <h4 class="white-text light center">Team Detail</h4>
+    <div class="card-panel teal modal-scroll">
+      <h4 class="white-text light center modal-mark">Team Detail</h4>
       <hr>
       <div class="center">
         <img class="modal-flag-image" src="./assets/images/not-found.svg" alt="Country Flag">
       </div> <!-- center -->
       <div class="center">
         <div class="card-panel">
-          <h5 class="light">Name: -</h5>
+          <h5 class="light team-name">Name: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Tla: -</h5>
+          <h5 class="light team-tla">Tla: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Adress: -</h5>
+          <h5 class="light team-address">Adress: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Phone: -</h5>
+          <h5 class="light team-phone">Phone: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Website: -</h5>
+          <h5 class="light team-website">Website: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Email: -</h5>
+          <h5 class="light team-email">Email: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Founded: -</h5>
+          <h5 class="light team-founded">Founded: -</h5>
         </div>
         <div class="card-panel">
-          <h5 class="light">Club Colors: -</h5>
+          <h5 class="light team-colors">Club Colors: -</h5>
         </div> <!-- card-panel -->
       </div> <!-- center -->
     </div> <!-- card-panel teal-->
   </div> <!-- modal-content -->
   <div class="modal-footer">
     <a class="waves-effect waves-light btn indigo accent-4">Save Team</a>
-    <a class="waves-effect waves-light btn red modal-close">Close</a>
+    <a class="waves-effect waves-light btn red close-modal-btn">Close</a>
   </div> <!-- modal-footer -->
   </div> <!-- modal modal-fixed-footer -->
 `;
--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,23 @@ const getTeamListData = () => {
 };
 
 const modalTeamDetail = (teamData) => {
-  
+  let teamName = document.querySelector(".team-name");
+  let teamTla = document.querySelector(".team-tla");
+  let teamAddress = document.querySelector(".team-address");
+  let teamPhone = document.querySelector(".team-phone");
+  let teamWebsite = document.querySelector(".team-website");
+  let teamEmail = document.querySelector(".team-email");
+  let teamFounded = document.querySelector(".team-founded");
+  let teamColors = document.querySelector(".team-colors");
+
+  teamName.innerHTML = "Name: <strong>" + teamData.name + "</strong>";
+  teamTla.innerHTML = "Tla: <strong>" + teamData.tla + "</strong>";
+  teamAddress.innerHTML = "Address: <strong>" + teamData.address + "</strong>";
+  teamPhone.innerHTML = "Phone: <strong>" + teamData.phone + "</strong>";
+  teamWebsite.innerHTML = "Website: <strong>" + teamData.website + "</strong>";
+  teamEmail.innerHTML = "Email: <strong>" + teamData.email + "</strong>";
+  teamFounded.innerHTML = "Founded: <strong>" + teamData.founded + "</strong>";
+  teamColors.innerHTML = "Club Colors: <strong>" + teamData.clubColors + "</strong>";
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -69,9 +85,24 @@ document.addEventListener('DOMContentLoaded', function() {
   // modal
   let modalElem = document.querySelectorAll('.modal');
   let modalInstances = M.Modal.init(modalElem, {
-    preventScrolling: true
-  })
+    preventScrolling: true,
+  });
+  let modalClose = document.querySelector(".close-modal-btn");
+  modalClose.addEventListener("click", function(e) {
+    e.preventDefault();
+    // document.querySelector(".modal-content").scrollTop = 0;
+    console.log(document.querySelector(".modal-content").scrollTop);
+    M.Modal.getInstance(modalElem[0]).close();
+  });
+  let modalContent = document.querySelector(".modal-content");
+  modalContent.addEventListener("scroll", function (event) {
+    var scroll = modalContent.scrollTop;
+    console.log("Scroll Top: " + modalContent.scrollTop);
+    console.log("Offset: " + modalContent.offsetTop);
+  });
 });
+
+
 
 // changing the content
 window.addEventListener('hashchange', function() {
