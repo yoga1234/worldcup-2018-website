ee2d0760ee7d63773563c4e6f07dc025d96b5d72
Septiana Yoga <septianayoga30@gmail.com>
2020-08-05 15:39:59 +0700

    add initial indexeddb

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,39 +51,6 @@ const getTeamListData = () => {
   });
 };
 
-// const modalTeamDetail = (teamData) => {
-//   let countryFlag = document.querySelector(".modal-flag-image");
-//   let teamName = document.querySelector(".team-name");
-//   let teamTla = document.querySelector(".team-tla");
-//   let teamAddress = document.querySelector(".team-address");
-//   let teamPhone = document.querySelector(".team-phone");
-//   let teamWebsite = document.querySelector(".team-website");
-//   let teamEmail = document.querySelector(".team-email");
-//   let teamFounded = document.querySelector(".team-founded");
-//   let teamColors = document.querySelector(".team-colors");
-
-//   // check for country flag
-//   if(teamData.crestUrl == null || teamData.crestUrl == "") {
-//     teamData.crestUrl = './assets/images/not-found.svg';
-//   }
-
-//   // check for email
-//   if(teamData.email == null || teamData.email == "") {
-//     teamEmail.innerHTML = "Email: <i>Email not available</i>";
-//   } else {
-//     teamEmail.innerHTML = "Email: <strong>" + teamData.email + "</strong>";
-//   }
-
-//   countryFlag.src = teamData.crestUrl;
-//   teamName.innerHTML = "Name: <strong>" + teamData.name + "</strong>";
-//   teamTla.innerHTML = "Tla: <strong>" + teamData.tla + "</strong>";
-//   teamAddress.innerHTML = "Address: <strong>" + teamData.address + "</strong>";
-//   teamPhone.innerHTML = "Phone: <strong>" + teamData.phone + "</strong>";
-//   teamWebsite.innerHTML = "Website: <strong>" + teamData.website + "</strong>";
-//   teamFounded.innerHTML = "Founded: <strong>" + teamData.founded + "</strong>";
-//   teamColors.innerHTML = "Club Colors: <strong>" + teamData.clubColors + "</strong>";
-// }
-
 document.addEventListener('DOMContentLoaded', function() {
   document.body.insertAdjacentHTML("afterbegin", NAVBAR);
   document.body.insertAdjacentHTML("beforeend", MODAL);
@@ -102,6 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let modalInstances = M.Modal.init(modalElem, {
     preventScrolling: true,
   });
+  // close modal button
   let modalClose = document.querySelector(".close-modal-btn");
   modalClose.addEventListener("click", function(e) {
     e.preventDefault();
@@ -109,7 +77,12 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(document.querySelector(".modal-content").scrollTop);
     M.Modal.getInstance(modalElem[0]).close();
   });
-  
+
+  // modal save button
+  let modalSave = document.querySelector(".save-modal-btn");
+  modalSave.addEventListener("click", function(e) {
+    console.log("Tombol save telah di klik.")
+  });
 });
 
 
